06d8655f5a44df13faac3688c5caddf8d64bc5f9
yrysbek14 <myrzakadyrovbook@gmail.com>
2025-12-06 13:33:22 +0600

    FinishDocker

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -133,6 +133,8 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
